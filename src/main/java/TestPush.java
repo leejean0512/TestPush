3f9ab7a579284de477cea333d5f69a593a662c62
lee <leejean0512_520@163.com>
2021-10-11 15:56:01 +0800

    测试Push!

---
+++ src/main/java/TestPush.java
@@ -0,0 +1,13 @@
+/**
+ * @Description: 测试push
+ * @Package: PACKAGE_NAME
+ * @ClassName: TestPush
+ * @Author: lee
+ * @CreateTime: 2021/10/11 15:34
+ * @Version: 1.0
+ */
+public class TestPush {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
